5bb827a5cefb66349e657fa82e1e1c6708bd138b
oregoneyes <russellsmailbox@yahoo.com>
2021-09-07 14:02:20 -0700

    modified plot 2

--- elec.R
+++ elec.R
@@ -14,17 +14,25 @@ elec <- tibble(elec)
 
 # convert Date and Time to actual R date and time objects.
 
-elec$Time <- paste(elec$Date, elec$Time)
+
 
 form <- '%d/%m/%Y'
 elec$Date <- as.Date(elec$Date, format = form)
 
-tim <- '%Y-%m-%d %H:%M:%S'
-red_elec$Time <- strptime(red_elec$Time, tim)
+
 
 # filter for dates of interest
 
 red_elec <- elec %>% filter(Date == '2007-02-01' | Date == '2007-02-02')
+
+red_elec$Time <- paste(red_elec$Date, red_elec$Time)
+
+x <- red_elec
+x$Time <- strptime(x$Time, format = tim)
+
+tim <- '%Y-%m-%d %H:%M:%S'
+red_elec$Time <- strptime(red_elec$Time, format = tim)
+
 
 # Plot 1
 
@@ -33,6 +41,6 @@ plot1 <- hist(as.numeric(red_elec$Global_active_power), main = 'Global Active Po
 
 # plot 2
 
-plot(red_elec$Time, as.numeric(red_elec$Global_active_power)
+plot(red_elec$Time, as.numeric(red_elec$Global_active_power), type = 'l')
 
      
